40ff2b51273c461d8920122af8e6692e39675e59
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-02 20:29:56 +0530

    Constellation Background

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import ParticlesComponent from './components/particles';
+import './App.css'
+function App() {
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <ParticlesComponent id='particles' />
+    </div>
+  )
+}
+
+export default App;
